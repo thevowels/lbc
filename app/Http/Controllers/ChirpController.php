019747bede0f2365acaf09ec234a16499686f75b
thevowels <aungphyohtet1@gmail.com>
2025-01-31 04:25:53 +0700

    Adding relationship between User and Chirps, allow to add message, artisan migrate and testing with Artisan Tinker

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -31,9 +32,15 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+        
+        return redirect(route('chirps.index'));
     }
 
     /**
