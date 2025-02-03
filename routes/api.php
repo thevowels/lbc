4d19c70206bcb2e141f01a73cf9d01d4bc98131d
thevowels <aungphyohtet1@gmail.com>
2025-02-03 19:50:58 +0700

    handle post

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return 'This is User API';
 });
+
+Route::post('/user', function(Request $request) {
+    return $request->all();
+});
